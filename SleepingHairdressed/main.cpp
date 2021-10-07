d083f315a456526eb6638aa34ac827946bc14c22
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-10-07 21:39:35 +0300

    Initial Commit

---
+++ SleepingHairdressed/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SleepingHairdressed
+//
+//  Created by ПК on 07.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
